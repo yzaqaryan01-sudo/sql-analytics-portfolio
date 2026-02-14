13a8ff2c91068973a3d6e3490fb678ff5d51a26f
yzaqaryan01-sudo <y.zaqaryan01@gmail.com>
2026-02-14 16:46:12 +0400

    adding final project_schema

--- init/10_project_schema.sql
+++ init/10_project_schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE project.neighbourhoods(
 neighbourhood_group TEXT,
-neighbourhood TEXT NOT NULL PRIMARY KEY
+neighbourhood TEXT NOT NULL 
 );
 CREATE TABLE project.listings (
-"id" INT NOT NULL PRIMARY KEY,
+"id" BIGINT NOT NULL PRIMARY KEY,
 "name" TEXT NOT NULL,
 host_id INT NOT NULL,
-host_name TEXT NOT NULL,
+host_name TEXT ,
 neighbourhood_group TEXT,
-neighbourhood TEXT NOT NULL REFERENCES project.neighbourhoods (neighbourhood),
+neighbourhood TEXT ,
 latitude DECIMAL(10,4) NOT NULL,
 longitude DECIMAL(10,4) NOT NULL,
 room_type TEXT,
@@ -19,7 +19,29 @@ last_review DATE,
 reviews_per_month DOUBLE PRECISION,
 calculated_host_listings_count INT,
 availability_365 INT,
-number_of_reviews_ltm INT);
+number_of_reviews_ltm INT,
+license TEXT);
 CREATE TABLE project.reviews(
-"id" INT NOT NULL REFERENCES project.listings("id"),
+"id" BIGINT NOT NULL REFERENCES project.listings("id"),
 "date" DATE);
+CREATE TABLE project.neighbourhoods_boundaries (
+    neighbourhood TEXT  ,
+    geom       GEOMETRY(MultiPolygon, 4326)
+);
+CREATE TABLE project._stg_neighbourhoods_boundaries (
+    neighbourhood TEXT,
+	neighbourhood_group TEXT,
+    wkt TEXT);
+COPY  project.neighbourhoods
+FROM '/data/neighbourhoods.csv'
+CSV HEADER;
+COPY  project.listings
+FROM '/data/listings.csv'
+CSV HEADER;
+
+COPY  project._stg_neighbourhoods_boundaries
+FROM '/data/neighbourhoods_wkt.csv'
+CSV HEADER;
+COPY  project.reviews
+FROM '/data/reviews.csv'
+CSV HEADER;
